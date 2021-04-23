1eacb16e5647de20061e8a764591f53e92f45e92
Marina Oliveira <marinaoliveira.r07@gmail.com>
2021-04-22 21:20:15 -0300

    feat: menu.

--- src/components/header.js
+++ src/components/header.js
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../assets/avatar_parking.png";
 import Hamburguer from "../assets/menu.png";
+import Close from "../assets/close.png";
+import Menu from "./menu";
 
 export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <header>
       <nav>
         <img src={Logo} alt="Parking Logo" />
-        <img src={Hamburguer} alt="Menu" />
+        <img
+          src={isOpen ? Close : Hamburguer}
+          alt="Menu"
+          onClick={() => {
+            setIsOpen(!isOpen);
+          }}
+        />
       </nav>
+
+      {isOpen ? <Menu /> : ""}
     </header>
   );
 }
--- src/components/menu.js
+++ src/components/menu.js
@@ -1,5 +1,12 @@
 import React from "react";
 
 export default function Menu() {
-  return <div></div>;
+  return (
+    <div className="Menu">
+      <ul>
+        <li>Entrance</li>
+        <li>Exit</li>
+      </ul>
+    </div>
+  );
 }
