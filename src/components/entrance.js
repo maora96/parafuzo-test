8f22ff03d3e4616f91b458fd8a224e10082b0d44
Marina Oliveira <marinaoliveira.r07@gmail.com>
2021-04-22 21:45:28 -0300

    feat: loading display for registering plates.

--- src/components/entrance.js
+++ src/components/entrance.js
@@ -1,34 +1,53 @@
 import React, { useState } from "react";
 import { fetchWithBody } from "../utils/fetch";
+import Loading from "../assets/loading.png";
+import Done from "../assets/done.png";
+
 export default function Entrance() {
   const [input, setInput] = useState("");
+  const [isLoading, setIsLoading] = useState(1);
+
   return (
     <div className="entrance-body">
-      <p>Número da placa:</p>
-      <input
-        type="text"
-        placeholder="AAA-0000"
-        onChange={(e) => {
-          setInput(e.target.value);
-        }}
-      />
+      {isLoading === 0 ? (
+        <>
+          <img src={Loading} alt="" /> <p class="center-me">Registrando...</p>
+        </>
+      ) : isLoading === 1 ? (
+        <>
+          <p>Número da placa:</p>
+          <input
+            type="text"
+            placeholder="AAA-0000"
+            onChange={(e) => {
+              setInput(e.target.value);
+            }}
+          />
 
-      <button
-        disabled={input === "" ? true : false}
-        onClick={() => {
-          fetchWithBody(
-            "https://parking-lot-to-pfz.herokuapp.com/parking",
-            "POST",
-            { plate: input }
-          )
-            .then((res) => res.json())
-            .then((resJson) => {
-              console.log(resJson);
-            });
-        }}
-      >
-        Confirmar entrada
-      </button>
+          <button
+            disabled={input === "" ? true : false}
+            onClick={() => {
+              setIsLoading(1);
+              fetchWithBody(
+                "https://parking-lot-to-pfz.herokuapp.com/parking",
+                "POST",
+                { plate: input }
+              )
+                .then((res) => res.json())
+                .then((resJson) => {
+                  console.log(resJson);
+                  setIsLoading(2);
+                });
+            }}
+          >
+            Confirmar entrada
+          </button>
+        </>
+      ) : (
+        <>
+          <img src={Done} alt="" /> <p class="center-me">REGISTRADO!</p>
+        </>
+      )}
     </div>
   );
 }
